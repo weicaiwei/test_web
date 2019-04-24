cc78eb178e230aa69a4e858bc3ded5b700b18c75
caiwei <1750886898@qq.com>
2019-04-24 13:53:44 +0800

    add demo about annotation @Valid @Validated and global exception execute @ControllerAdvice @ExceptionHandler

--- src/main/java/com/caiwei/demo/global/NowApplicationContext.java
+++ src/main/java/com/caiwei/demo/global/NowApplicationContext.java
@@ -2,7 +2,6 @@
 package com.caiwei.demo.global;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
