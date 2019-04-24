6379ab7c7ab1d92f1526d47649bfd0ae5461fc06
caiwei <1750886898@qq.com>
2019-04-24 14:33:23 +0800

    hello git branch

---
+++ src/main/java/com/caiwei/demo/controller/HelloGit.java
@@ -0,0 +1,17 @@
+package com.caiwei.demo.controller;
+
+/**
+ * @ClassName: HelloGit
+ * @Description: TODO
+ * @auther: caiwei
+ * @date: 2019/4/24 14:25
+ */
+public class HelloGit {
+
+    private String hello;
+
+    public void setHello() {
+        System.out.println(hello);
+
+    }
+}
